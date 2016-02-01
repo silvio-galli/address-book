58773e821577b600938b83b3f4ec17b46482ce7e
silvio-galli <silvio.galli@gmail.com>
2016-02-01 16:05:03 +0100

    Linked menu and submenu in controllers/menu_controller.rb to right functions in models/address_book.rb

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -51,7 +51,6 @@ class MenuController
     @address_book.entries.each do |entry|
       system "clear"
       puts entry.to_s
-
       entry_submenu(entry)
     end
     system "clear"
@@ -75,9 +74,64 @@ class MenuController
   end
 
   def search_entries
+    print "Search by name: "
+    name = gets.chomp
+
+    match = @address_book.binary_search(name)
+    system "clear"
+
+    if match
+      puts match.to_s
+      search_submenu(match)
+    else
+      puts "No match found for #{name}"
+    end
+  end
+
+  def search_submenu(entry)
+    puts "\nd - delete this entry"
+    puts "e - edit this entry"
+    puts "m - return to main menu"
+    selection = gets.chomp
+
+    case selection
+    when "d"
+      system "clear"
+      delete_entry(entry)
+      main_menu
+    when "e"
+      edit_entry(entry)
+      system "clear"
+      main_menu
+    when "m"
+      system "clear"
+      main_menu
+    else
+      system "clear"
+      puts "#{selection} is not a valid input"
+      puts entry.to_s
+      search_submenu(entry)
+    end
   end
 
   def read_csv
+    print "Enter CSV file to import:"
+    file_name = gets.chomp
+
+    if file_name.empty?
+      system "clear"
+      puts "No CSV file read"
+      main_menu
+    end
+
+    begin
+      entry_count = @address_book.import_from_csv(file_name).count
+      system "clear"
+      puts "#{entry_count} entries added from #{file_name}"
+    rescue
+      puts "#{file_name} is not a valid CSV file, please enter a valid CSV file."
+      read_csv
+    end
   end
 
   def entry_submenu(entry)
@@ -91,7 +145,10 @@ class MenuController
     case selection
     when "n"
     when "d"
+      delete_entry(entry)
     when "e"
+      edit_entry(entry)
+      entry_submenu(entry)
     when "m"
       system "clear"
       main_menu
@@ -101,6 +158,28 @@ class MenuController
       puts "Please, try again"
       entry_submenu(entry)
     end
+  end
+
+  def delete_entry(entry)
+    @address_book.entries.delete(entry)
+    puts "#{entry.name} has been deleted"
+  end
+
+  def edit_entry(entry)
+    print "Update '#{entry.name}' to: "
+    name = gets.chomp
+    print "Update #{entry.phone_number} to: "
+    phone_number = gets.chomp
+    print "Update #{entry.email} to: "
+    email = gets.chomp
+
+    entry.name = name if !name.empty?
+    entry.phone_number = phone_number if !phone_number.empty?
+    entry.email = email if !email.empty?
+
+    system "clear"
+    puts "Updated entry: "
+    puts entry
   end
 
 end
